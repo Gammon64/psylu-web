3a620d5aef42b05a8076a28795e140eebd294aad
Hugo Henrique <hugo.hbs64@gmail.com>
2026-04-22 15:07:04 -0300

    fix(auth): protegendo todas as rotas

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,1 +1,20 @@
-export { auth as proxy } from "@/lib/auth";
+import { auth } from "@/lib/auth"; // seu auth.ts do next-auth
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+
+  const isPublicRoute = req.nextUrl.pathname === "/login";
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  if (isLoggedIn && isPublicRoute) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
